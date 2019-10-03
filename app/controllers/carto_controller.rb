052b3cabfa14c5181a47cf530f4d0690ef6cf9bc
Brad Smith <brad@smithwebtek.com>
2019-10-03 10:48:35 -0400

    Set up diversity route and json

---
+++ app/controllers/carto_controller.rb
@@ -0,0 +1,11 @@
+class CartoController < ApplicationController
+
+  require_relative '../data/diversity'
+  include DiversityJson
+
+
+  def diversity
+    render json: DIVERSITY_JSON
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get '/http://127.0.0.1:3000/diversity%20muni_id,nhwhi_00p,nhwhi_10p,nhwh_pdif,nhaa_pdif,lat_pdif,nhas_pdif,municipal%20FROM%20demo_pop_race_00_10m', to: 'carto#diversity'
+  get '/diversity', to: 'carto#diversity'
 end
