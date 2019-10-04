260c152e678e37dd7315ce37f901e53b9172a34e
Brad Smith <brad@smithwebtek.com>
2019-10-04 11:14:46 -0400

    Add API data files, controller actions, routes

---
+++ app/data/employment/employment4.rb
@@ -0,0 +1,4 @@
+module Employment4
+
+EMPLOYMENT4 = {"rows":[{"msa_id":14460,"nhwue_p":7.13,"aaue_p":15.63,"asue_p":7.83,"latue_p":13}],"time":0.062,"fields":{"msa_id":{"type":"number","pgtype":"int4"},"nhwue_p":{"type":"number","pgtype":"numeric"},"aaue_p":{"type":"number","pgtype":"numeric"},"asue_p":{"type":"number","pgtype":"numeric"},"latue_p":{"type":"number","pgtype":"numeric"}},"total_rows":1}
+end
---
+++ app/data/income/income4.rb
@@ -0,0 +1,5 @@
+module Income4
+
+  INCOME4 =
+  {"rows":[{"msa_id":"14460","mhi":75389,"nhw_mhi":82659,"aa_mhi":43986,"as_mhi":80784,"lat_mhi":41507}],"time":0.005,"fields":{"msa_id":{"type":"string","pgtype":"text"},"mhi":{"type":"number","pgtype":"numeric"},"nhw_mhi":{"type":"number","pgtype":"numeric"},"aa_mhi":{"type":"number","pgtype":"numeric"},"as_mhi":{"type":"number","pgtype":"numeric"},"lat_mhi":{"type":"number","pgtype":"numeric"}},"total_rows":1}
+end
